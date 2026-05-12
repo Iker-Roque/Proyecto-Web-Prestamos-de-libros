2b54fc49aed5e0c2fd64e3d1be0bce8dc7b47074
Iker-Roque <72403549@mail.isil.pe>
2026-05-12 13:58:37 -0500

    Version2

--- src/main/java/pe/edu/isil/gestion_prestamos/controller/LoginController.java
+++ src/main/java/pe/edu/isil/gestion_prestamos/controller/LoginController.java
@@ -1,6 +1,7 @@
 package pe.edu.isil.gestion_prestamos.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +21,18 @@ public class LoginController {
     @PostMapping("/login")
     public ResponseEntity<Usuario> login(@RequestBody LoginDTO dto) {
         Usuario usuario = usuarioService.login(dto);
-        return ResponseEntity.ok(usuario);
+        if(usuario == null) {
+            return ResponseEntity.ok(usuario);
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
     }
 
     @PostMapping("/registrar")
     public ResponseEntity<Usuario> registrar(@RequestBody Usuario usuario) {
-        return ResponseEntity.ok(usuarioService.registrar(usuario));
+        Usuario nuevoUsuario = usuarioService.registrar(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoUsuario); //201 Created
     }
     
    
